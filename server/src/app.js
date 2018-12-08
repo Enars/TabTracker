a5bb6161cfbed6dcee76bf349d0f57b90ab74363
Enars <ejnar.sorensen@gmail.com>
2018-12-08 16:14:45 +0100

    fixed the sqlite database by installing the sqlite3 module

--- server/src/app.js
+++ server/src/app.js
@@ -2,24 +2,26 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const { sequelize } = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-let myUser = 'was not set'
+/* let myUser = 'was not set' */
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: 'Dear ' + req.body.email + ', your user was registered'
-  })
-})
-
-app.get('/myUser', (req, res) => {
+/* app.get('/myUser', (req, res) => {
   res.send({
     message: 'Your user name is: ' + myUser
   })
-})
+}) */
 
-app.listen(process.env.PORT || 8081)
+require('./routes')(app)
+
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log('Running server on port ' + config.port)
+  })
